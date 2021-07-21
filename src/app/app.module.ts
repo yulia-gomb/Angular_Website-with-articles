2e7c5e34bc987d434f7a4c47446ce6efa6655714
unknown <juliesg@tut.by>
2021-07-21 21:47:54 +0300

    Main page: layout created, tags added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,36 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { MainPageComponent } from './main-page/main-page.component';
+import { TagsComponent } from './tags/tags.component';
+
+import "firebase/app";
+import "firebase/auth";
+import "firebase/storage";
+import "firebase/database";
+import firebase from "firebase";
+
+export const firebaseConfig = {
+  apiKey: "AIzaSyATt36GJHPvqSKNaTIcdXpU47Xdv0_Ofmg",
+  authDomain: "leverx-745ad.firebaseapp.com",
+  projectId: "leverx-745ad",
+  storageBucket: "leverx-745ad.appspot.com",
+  messagingSenderId: "33580969644",
+  appId: "1:33580969644:web:b12ab304545fb49a5ab909",
+  databaseURL: "https://leverx-745ad-default-rtdb.europe-west1.firebasedatabase.app/"
+};
+
+firebase.initializeApp(firebaseConfig);
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    MainPageComponent,
+    TagsComponent
   ],
   imports: [
     BrowserModule,
