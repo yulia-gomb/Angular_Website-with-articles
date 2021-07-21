544242824c37214597772f6c6c24b7ae461fdcc5
unknown <juliesg@tut.by>
2021-07-22 00:26:59 +0300

    Main page: articles are added from the server

--- src/app/main-page/main-page.component.ts
+++ src/app/main-page/main-page.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import firebase from "firebase";
+
 
 @Component({
   selector: 'app-main-page',
@@ -6,10 +8,26 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./main-page.component.css']
 })
 export class MainPageComponent implements OnInit {
+  data: any;
+
+
 
   constructor() { }
 
   ngOnInit(): void {
+
+    // getting data from Firebase
+
+    firebase.database().ref().on('value', (snap) => {
+      this.data = Object.entries(snap.val().articles);
+      console.log(this.data);
+
+
+    })
+
+
+
+
   }
 
 }
