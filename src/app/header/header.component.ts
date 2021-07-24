9716b3f6e907cc11d8a6b2e3841341656abc5f1f
unknown <juliesg@tut.by>
2021-07-25 00:04:08 +0300

    Button Log-out activated

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { AngularFireAuth } from '@angular/fire/auth';
+import firebase from 'firebase/app';
 
 @Component({
   selector: 'app-header',
@@ -7,9 +9,48 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HeaderComponent implements OnInit {
 
-  constructor() { }
+  showButtonCreateAPost!: boolean;
+  showButtonSignIn!: boolean;
+  showButtonLogOut!: boolean;
+  showAvatar!: boolean;
+
+  avatarImg: string | undefined;
+
+  constructor(public auth: AngularFireAuth) { }
+
+  logOut() {
+    this.auth.signOut().then( () => {
+      delete localStorage.authorized;
+      delete localStorage.avatar;
+      delete localStorage.author;
+      this.showButtonCreateAPost = false;
+      this.showButtonSignIn = true;
+      this.showButtonLogOut = false;
+      this.showAvatar = false;
+
+    }).catch(function (err) {
+      console.log("error")
+      console.log(err)
+    });
+  }
 
   ngOnInit(): void {
+    this.avatarImg = localStorage.avatar;
+    if(localStorage.getItem('authorized')){
+      this.showButtonSignIn = false;
+      this.showButtonCreateAPost = true;
+      this.showButtonLogOut = true;
+      this.showAvatar = true;
+    }
+    else{
+      this.showButtonCreateAPost = false;
+      this.showButtonSignIn = true;
+      this.showButtonLogOut = false;
+      this.showAvatar = false;
+    }
+
   }
+
+
 
 }
--- src/app/page-log-in/page-log-in.component.ts
+++ src/app/page-log-in/page-log-in.component.ts
@@ -9,22 +9,30 @@ import firebase from "firebase";
 })
 export class PageLogInComponent implements OnInit {
 
+  buttonAble: boolean = true;
+
   constructor(public auth: AngularFireAuth) {
       }
       //authentication of user
-  logIn(){
-    console.log('login')
-    this.auth.signInWithPopup(new firebase.auth.GoogleAuthProvider()).then(function (result : any) {
-      localStorage.setItem('authorized', 'true');
-      localStorage.setItem('avatar', result.additionalUserInfo.profile.picture);
-      localStorage.setItem('author', result.additionalUserInfo.profile.name);
-      window.location.href="../../index.html";
+        logIn(){
+          console.log('login')
+          this.auth.signInWithPopup(new firebase.auth.GoogleAuthProvider()).then(function (result : any) {
+            localStorage.setItem('authorized', 'true');
+            localStorage.setItem('avatar', result.additionalUserInfo.profile.picture);
+            localStorage.setItem('author', result.additionalUserInfo.profile.name);
+            window.location.href="../../index.html";
 
-    }).catch(function (err) {
-      console.log("error")
-      console.log(err)
-    });
-  }
+          }).catch(function (err) {
+            console.log("error")
+            console.log(err)
+          });
+        }
+
+      //checking of "google terms" to activate button "sign In"
+
+      checkStatus() {
+          this.buttonAble = !this.buttonAble;
+      }
 
 
   ngOnInit(): void {
